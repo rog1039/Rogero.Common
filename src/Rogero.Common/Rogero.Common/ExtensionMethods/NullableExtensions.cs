2fe82ff71251bb037141225b5e7ea67e2f5bf4b2
Paul Rogero <progero@gmail.com>
2016-11-28 19:41:57 -0500

    Added some extensions.

---
+++ src/Rogero.Common/Rogero.Common/ExtensionMethods/NullableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rogero.Common.ExtensionMethods
+{
+    public static class NullableExtensions
+    {
+        public static T ValueOrThis<T>(T? val, T other) where T : struct
+        {
+            if (val.HasValue) return val.Value;
+            return other;
+        }
+    }
+
+}
---
+++ src/Rogero.Common/Rogero.Common/ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rogero.Common.ExtensionMethods
+{
+    public static class StringExtensions
+    {
+        public static string Bracketize(this string input)
+        {
+            return "[" + input + "]";
+        }
+
+        public static string Bracify(this string input)
+        {
+            return "{" + input + "}";
+        }
+
+        public static string Repeat(this string input, int repeatCount)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < repeatCount; i++)
+            {
+                sb.Append(input);
+            }
+            return sb.ToString();
+        }
+        public static bool InsensitiveEquals(this string s1, string s2)
+        {
+            var oneIsNullOtherIsnt = (s1 == null && s2 != null) || (s1 != null && s2 == null);
+            if (oneIsNullOtherIsnt)
+                return false;
+
+            var bothAreNull = s1 == null && s2 == null;
+            if (bothAreNull)
+                return true;
+
+            return s1.Length == s2.Length && s1.IndexOf(s2, StringComparison.InvariantCultureIgnoreCase) == 0;
+        }
+        public static bool InsensitiveContains(this string input, string part)
+        {
+            return CultureInfo.InvariantCulture.CompareInfo.IndexOf(input, part, CompareOptions.IgnoreCase) >= 0;
+        }
+
+        public static string RemoveAllWhitespace(this string s)
+        {
+            var result = s.Replace("\r\n", "");
+            result = result.Replace("\r", "");
+            result = result.Replace("\n", "");
+            result = result.Replace("\t", "");
+            
+            return result;
+        }
+    }
+}
