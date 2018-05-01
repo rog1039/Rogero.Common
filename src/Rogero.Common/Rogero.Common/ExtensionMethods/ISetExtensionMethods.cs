cc57f19bfe8209511a73289ad70814d431f5e31e
Paul Rogero <paulrogero@woebermustard.com>
2018-05-01 15:28:18 -0400

    More work on claim management.

---
+++ src/Rogero.Common/Rogero.Common/ExtensionMethods/ISetExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Rogero.Common.ExtensionMethods
+{
+    public static class ISetExtensionMethods
+    {
+        public static void AddRange<T>(this ISet<T> set, IEnumerable<T> objects)
+        {
+            foreach (var obj in objects)
+            {
+                set.Add(obj);
+            }
+        }
+    }
+}
