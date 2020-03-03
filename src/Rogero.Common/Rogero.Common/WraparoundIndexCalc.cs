dad850c3bb59d76d1a716e2d19efd52eb2a82236
Paul Rogero <progero@gmail.com>
2020-03-03 15:02:52 -0500

    Added WrapAround index calculator.

---
+++ src/Rogero.Common/Rogero.Common/WraparoundIndexCalc.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+
+namespace Rogero.Common
+{
+    public static class WraparoundIndexCalc
+    {
+        public static int PreviousIndex(this IList list, int currentIndex)
+        {
+            return PreviousIndex(list.Count, currentIndex);
+        }
+
+        public static int NextIndex(this IList list, int currentIndex)
+        {
+            return NextIndex(list.Count, currentIndex);
+        }
+
+        public static int PreviousIndex(int listSize, int currentIndex)
+        {
+            return currentIndex <= 0
+                ? GetFinalIndex(listSize)
+                : currentIndex - 1;
+        }
+
+        public static int NextIndex(int listSize, int currentIndex)
+        {
+            return currentIndex >= GetFinalIndex(listSize)
+                ? 0
+                : currentIndex + 1;
+        }
+
+        public static int GetFinalIndex(int listSize)
+        {
+            return listSize - 1;
+        }
+    }
+}
