a704f12a1816e8a53e6aac778779d6dc137b4ca5
Paul Rogero <progero@gmail.com>
2020-07-20 16:29:33 -0400

    More work on ItemMap feature. New sync system looks good.

--- src/Rogero.Common/Rogero.Common/Infrastructure/ObjectTextSearcher.cs
+++ src/Rogero.Common/Rogero.Common/Infrastructure/ObjectTextSearcher.cs
@@ -16,7 +16,7 @@ namespace Rogero.Common
             return ObjectTextSearcher.FindMatches(items, searchText);
         }
     }
-    
+
     public class ObjectTextSearcher
     {
         private static readonly ConcurrentDictionary<Type, PropertyInfo[]> PropertyInfoMap =
@@ -39,7 +39,8 @@ namespace Rogero.Common
                             break;
                         }
                     }
-                    if(itemMatches) yield return item;
+
+                    if (itemMatches) yield return item;
                 }
 
                 yield break;
@@ -52,27 +53,34 @@ namespace Rogero.Common
             }
         }
 
-        public static bool Search(object obj, string searchText)
+        public static bool Search(object obj, string searchText, IList<Func<object, string, bool?>> additionalSearchTypeFuncs = null)
         {
             var objectProperties = GetProperties(obj.GetType());
-            return Search(objectProperties, obj, searchText);
+            return Search(objectProperties, obj, searchText, additionalSearchTypeFuncs);
         }
 
-        public static bool Search(PropertyInfo[] properties, object obj, string searchText)
+
+        public static bool Search(PropertyInfo[] properties, object obj, string searchText, IList<Func<object, string, bool?>> additionalSearchTypeFuncs = null)
         {
-            if(string.IsNullOrWhiteSpace(searchText)) return true;
-            
+            if (string.IsNullOrWhiteSpace(searchText)) return true;
+
             var terms = searchText.Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries);
             foreach (var term in terms)
             {
-                var match = Matches(properties, obj, term, 0);
+            var match = Matches(properties, obj, term, 0, additionalSearchTypeFuncs:additionalSearchTypeFuncs);
                 if (!match) return false;
             }
+
             return true;
         }
+        
+        private static readonly List<Func<object, string, bool?>> EmptySearchFuncs = new List<Func<object, string, bool?>>();
 
-        private static bool Matches(PropertyInfo[] properties, object item, string searchText, int depth = 0, int maxDepth = 3)
+        private static bool Matches(PropertyInfo[] properties, object item, string searchText, int depth = 0, int maxDepth = 3, 
+                                    IList<Func<object, string, bool?>> additionalSearchTypeFuncs = null)
         {
+            additionalSearchTypeFuncs ??= EmptySearchFuncs;
+
             if (item == null) return false;
             if (string.IsNullOrWhiteSpace(searchText)) return true;
             if (depth > maxDepth) return false;
@@ -85,60 +93,79 @@ namespace Rogero.Common
 
             foreach (var propertyInfo in properties)
             {
-                var type = propertyInfo.PropertyType;
+                var type      = propertyInfo.PropertyType;
+                var propValue = propertyInfo.GetValue(item);
+
+                if (additionalSearchTypeFuncs.Count > 0)
+                {
+                    foreach (var additionalSearchTypeFunc in additionalSearchTypeFuncs)
+                    {
+                        var result = additionalSearchTypeFunc(propValue, searchText);
+                        if (result.HasValue) return resultTransform(result.Value);
+                    }
+                }
+
                 if (type == typeof(string))
                 {
-                    var value = (string)propertyInfo.GetValue(item);
+                    var value  = (string) propValue;
                     var result = value != null && value.InsensitiveContains(searchText);
                     if (result) return resultTransform(true);
                 }
                 else if (type == typeof(int))
                 {
-                    var value = ((int)propertyInfo.GetValue(item));
+                    var value  = (int) propValue;
                     var result = value.ToString().InsensitiveContains(searchText);
                     if (result) return resultTransform(true);
                 }
                 else if (type == typeof(decimal))
                 {
-                    var value = ((decimal)propertyInfo.GetValue(item));
+                    var value  = (decimal) propValue;
                     var result = value.ToString().InsensitiveContains(searchText);
                     if (result) return resultTransform(true);
                 }
                 else if (type == typeof(double))
                 {
-                    var value = ((double)propertyInfo.GetValue(item));
+                    var value  = (double) propValue;
                     var result = value.ToString().InsensitiveContains(searchText);
                     if (result) return resultTransform(true);
                 }
                 else if (type == typeof(DateTime))
                 {
-                    var value = ((DateTime) propertyInfo.GetValue(item));
+                    var value  = (DateTime) propValue;
                     var result = value.ToString("d").InsensitiveContains(searchText);
                     if (result) return resultTransform(true);
                 }
                 else if (type.ImplementsInterface<IEnumerable>())
                 {
-                    var value = (IEnumerable) propertyInfo.GetValue((item));
+                    var value = (IEnumerable) propValue;
                     foreach (var child in value)
                     {
-                        var childProperties = GetProperties(child.GetType());
+                        var childProperties  = GetProperties(child.GetType());
                         var childMatchResult = Matches(childProperties, child, searchText, depth + 1);
                         if (childMatchResult) return resultTransform(true);
                     }
                 }
                 else
                 {
-                    var val = propertyInfo.GetValue(item);
-                    var props = type.GetProperties();
-                    var result = Matches(props, val, searchText, depth + 1);
-                    if(result) return resultTransform(true);
+                    try
+                    {
+                        var props  = type.GetProperties();
+                        var result = Matches(props, propValue, searchText, depth + 1);
+                        if (result) return resultTransform(true);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        throw;
+                    }
                 }
             }
+
             return resultTransform(false);
         }
 
         private static bool DoNothingBoolTransform(bool b) => b;
-        private static bool InvertBoolTransform(bool b) => !b;
+        private static bool InvertBoolTransform(bool    b) => !b;
 
         private static PropertyInfo[] GetProperties(Type objectType)
         {
@@ -149,13 +176,13 @@ namespace Rogero.Common
 
     public class SearchTerm
     {
-        public bool NegativeSearch { get; }
-        public string TermText { get; }
+        public bool   NegativeSearch { get; }
+        public string TermText       { get; }
 
         private SearchTerm(bool negativeSearch, string termText)
         {
             NegativeSearch = negativeSearch;
-            TermText = termText;
+            TermText       = termText;
         }
 
         public static IList<SearchTerm> ExtractAllTerms(string searchText)
@@ -175,7 +202,7 @@ namespace Rogero.Common
             var isTermNegated = rawTerm.Length > 1 && rawTerm[0] == '-';
 
             return isTermNegated
-                ? new SearchTerm(true, rawTerm.Remove(0, 1))
+                ? new SearchTerm(true,  rawTerm.Remove(0, 1))
                 : new SearchTerm(false, rawTerm);
         }
 
