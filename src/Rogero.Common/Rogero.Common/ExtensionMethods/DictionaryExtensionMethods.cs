41e2839d8985c52de8f28ba7866ed8078a750c99
Paul Rogero <progero@gmail.com>
2020-11-19 15:09:37 -0500

    Added another TryGet method to Dictionary.

--- src/Rogero.Common/Rogero.Common/ExtensionMethods/DictionaryExtensionMethods.cs
+++ src/Rogero.Common/Rogero.Common/ExtensionMethods/DictionaryExtensionMethods.cs
@@ -124,5 +124,44 @@ namespace Rogero.Common.ExtensionMethods
                 dict.AddToDictionary(keyFunc(item), valueFunc(item));
             }
         }
+
+        public static DictionaryResult<TValue> TryGet<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key)
+        {
+            if(dict.TryGetValue(key, out var val))
+            {
+                return DictionaryResult<TValue>.Found(val);
+            }
+            return DictionaryResult<TValue>.NotFound();
+        }
+        
+    }
+
+    public class DictionaryResult<T>
+    {
+        private DictionaryResult()
+        {
+            WasFound = false;
+        }
+
+        private DictionaryResult(T value)
+        {
+            WasFound = true;
+            Value    = value;
+        }
+
+        public bool WasFound { get;  }
+        public T    Value     { get; }
+
+        public static DictionaryResult<T> Found(T value)
+        {
+            var result = new DictionaryResult<T>(value);
+            return result;
+        }
+
+        public static DictionaryResult<T> NotFound()
+        {
+            return new DictionaryResult<T>();
+        }
+        
     }
 }
