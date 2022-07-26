61c09fbd02ddc04b3c4f80b8f4e798575ba841a6
Paul Rogero <rog1039@users.noreply.github.com>
2022-07-26 16:24:09 -0400

    Added more methods to WraparoundIndexCalc.cs

--- src/Rogero.Common/Rogero.Common/WraparoundIndexCalc.cs
+++ src/Rogero.Common/Rogero.Common/WraparoundIndexCalc.cs
@@ -4,32 +4,46 @@ namespace Rogero.Common;
 
 public static class WraparoundIndexCalc
 {
-    public static int PreviousIndex(this IList list, int currentIndex)
-    {
-        return PreviousIndex(list.Count, currentIndex);
-    }
-
-    public static int NextIndex(this IList list, int currentIndex)
-    {
-        return NextIndex(list.Count, currentIndex);
-    }
-
-    public static int PreviousIndex(int listSize, int currentIndex)
+    private static int PreviousIndex(int listSize, int currentIndex)
     {
         return currentIndex <= 0
             ? GetFinalIndex(listSize)
             : currentIndex - 1;
     }
 
-    public static int NextIndex(int listSize, int currentIndex)
+    private static int NextIndex(int listSize, int currentIndex)
     {
         return currentIndex >= GetFinalIndex(listSize)
             ? 0
             : currentIndex + 1;
     }
 
-    public static int GetFinalIndex(int listSize)
+    private static int GetFinalIndex(int listSize)
     {
         return listSize - 1;
+    }
+
+    public static int PreviousIndex<T>(this IList<T> list, int currentIndex)
+    {
+        return PreviousIndex(list.Count, currentIndex);
+    }
+
+    public static int NextIndex<T>(this IList<T> list, int currentIndex)
+    {
+        return NextIndex(list.Count, currentIndex);
+    }
+    
+    public static T PreviousItem<T>(this IList<T> list, int index) => list[list.PreviousIndex(index)];
+    public static T NextItem<T>(this IList<T> list, int index) => list[list.NextIndex(index)];
+    
+    public static T PreviousItem<T>(this IList<T> list, T item)
+    {
+        var currentIndex = list.IndexOf(item);
+        return list.PreviousItem(currentIndex);
+    }
+    public static T NextItem<T>(this IList<T> list, T item)
+    {
+        var currentIndex = list.IndexOf(item);
+        return list.NextItem(currentIndex);
     }
 }
